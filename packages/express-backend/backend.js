f3020533b5445cf76b987fe5a29433405cde35bf
Aaron Krimer <akrimer@calpoly.edu>
2024-10-11 00:57:41 -0700

    added users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,12 +6,48 @@ const port = 8000;
 
 app.use(express.json());
 
+// Users list (data to be served at the /users endpoint)
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspiring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+//  send the users list as a response
+app.get("/users", (req, res) => {
+  res.send(users);  // Responds with the users list in JSON
 });
+
+// Start the server and listen on the  port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
