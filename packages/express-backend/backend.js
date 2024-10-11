888bdbf93b01885f132046e2c403a63e263d0eb4
Aaron Krimer <akrimer@calpoly.edu>
2024-10-11 01:16:22 -0700

    Added ability to filter users by name using query parameter

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -9,44 +9,39 @@ app.use(express.json());
 // Users list (data to be served at the /users endpoint)
 const users = {
   users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspiring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
+    { id: "xyz789", name: "Charlie", job: "Janitor" },
+    { id: "abc123", name: "Mac", job: "Bouncer" },
+    { id: "ppp222", name: "Mac", job: "Professor" },
+    { id: "yat999", name: "Dee", job: "Aspiring actress" },
+    { id: "zap555", name: "Dennis", job: "Bartender" }
   ]
 };
 
+// Find user by name function
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+// Default route that sends "Hello World!"
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-//  send the users list as a response
+// /users route that either returns the entire list of users or filters by name if a query parameter is provided
 app.get("/users", (req, res) => {
-  res.send(users);  // Responds with the users list in JSON
+  const name = req.query.name;
+  if (name !== undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
 
-// Start the server and listen on the  port
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
